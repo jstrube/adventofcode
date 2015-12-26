9aae517be8c4b896a4c11017790fbdb2f723cbc1
Jan Strube <jan.strube@gmail.com>
2015-12-25 16:08:34 -0800

    puzzle 19 part 1
    part 2 has a trick to it that depends on the input.
    Solving the problem in general is not trivial

---
+++ adventofcode19.jl
@@ -0,0 +1,64 @@
+replacements = split("""Al => ThF
+Al => ThRnFAr
+B => BCa
+B => TiB
+B => TiRnFAr
+Ca => CaCa
+Ca => PB
+Ca => PRnFAr
+Ca => SiRnFYFAr
+Ca => SiRnMgAr
+Ca => SiTh
+F => CaF
+F => PMg
+F => SiAl
+H => CRnAlAr
+H => CRnFYFYFAr
+H => CRnFYMgAr
+H => CRnMgYFAr
+H => HCa
+H => NRnFYFAr
+H => NRnMgAr
+H => NTh
+H => OB
+H => ORnFAr
+Mg => BF
+Mg => TiMg
+N => CRnFAr
+N => HSi
+O => CRnFYFAr
+O => CRnMgAr
+O => HP
+O => NRnFAr
+O => OTi
+P => CaP
+P => PTi
+P => SiRnFAr
+Si => CaSi
+Th => ThCa
+Ti => BP
+Ti => TiTi
+e => HF
+e => NAl
+e => OMg""", "\n")
+
+inpt = """ORnPBPMgArCaCaCaSiThCaCaSiThCaCaPBSiRnFArRnFArCaCaSiThCaCaSiThCaCaCaCaCaCaSiRnFYFArSiRnMgArCaSiRnPTiTiBFYPBFArSiRnCaSiRnTiRnFArSiAlArPTiBPTiRnCaSiAlArCaPTiTiBPMgYFArPTiRnFArSiRnCaCaFArRnCaFArCaSiRnSiRnMgArFYCaSiRnMgArCaCaSiThPRnFArPBCaSiRnMgArCaCaSiThCaSiRnTiMgArFArSiThSiThCaCaSiRnMgArCaCaSiRnFArTiBPTiRnCaSiAlArCaPTiRnFArPBPBCaCaSiThCaPBSiThPRnFArSiThCaSiThCaSiThCaPTiBSiRnFYFArCaCaPRnFArPBCaCaPBSiRnTiRnFArCaPRnFArSiRnCaCaCaSiThCaRnCaFArYCaSiRnFArBCaCaCaSiThFArPBFArCaSiRnFArRnCaCaCaFArSiRnFArTiRnPMgArF"""
+
+solutions = Set{ASCIIString}()
+push!(solutions, inpt) # to cover the case of no matches. Will be subtracted at the end
+subst = r"(\w+) => (\w+)"
+
+#part 1
+for line in replacements
+	m = match(subst, line)
+	for i in 1:length(inpt)
+		sol = inpt[1:i-1]*replace(inpt[i:end], m.captures[1], m.captures[2], 1)
+		push!(solutions, sol)
+	end
+end
+println(length(solutions)-1) # one is the original molecule, in case of no substitutions
+
+
+# part 2
+# solution from here https://www.reddit.com/r/adventofcode/comments/3xflz8/day_19_solutions/cy4etju
+# I can't figure this out ...
